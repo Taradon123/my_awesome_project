ed8cc67584b49a3950273d502b3c815cba38c08e
Taradon chueasucaritphaiboon <dram100aaa@gmail.com>
2024-07-06 14:47:39 +0700

    เพิ่มงาน

---
+++ lib/screen/second_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Screen'),
+      ),
+      body: Center(
+        child: Text('This is the second screen'),
+      ),
+    );
+  }
+}
